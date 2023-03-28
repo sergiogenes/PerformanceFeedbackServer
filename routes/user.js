6115287d7117c6e72461fa8fc3cd553c91dcf62c
NicoFJCruz <nicofjcruz@gmail.com>
2023-03-28 18:10:10 -0300

    corrección desactivar usuario

--- routes/user.js
+++ routes/user.js
@@ -14,7 +14,7 @@ router.get('/', validateAuth, allUser)
 router.get('/all', validateAuth, includeDeactivated) //include deactivated users
 router.get('/:id', validateAuth, oneUser)
 router.post('/', validateAuth, createUser)
-router.put('/deactivate/:id', validateAuth, deactivateUser)
 router.put('/:id', validateAuth, modifyUser)
+router.delete('/:id', validateAuth, deactivateUser)
 
 module.exports = router
