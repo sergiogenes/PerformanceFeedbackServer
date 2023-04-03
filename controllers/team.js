cb8d93eecb95dd8004fc8733f0a17e5c81ca2113
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-03 19:24:07 -0300

    Rutas equipos

---
+++ controllers/team.js
@@ -0,0 +1,72 @@
+const { ValidationError } = require('sequelize')
+const { User, Team } = require('../models')
+
+const allTeam = async (req, res, next) => {
+  try {
+    const team = await Team.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    })
+    res.send(team)
+  } catch (error) {
+    return res.send(console.error(error)).status(400)
+  }
+}
+
+const oneTeam = async (req, res, next) => {
+  try {
+    const team = await Team.findByPk(req.params.id, {
+      include: [{ model: User }],
+    })
+    res.send(team)
+  } catch (error) {
+    return res.send(console.error(error)).status(400)
+  }
+}
+
+const createTeam = async (req, res, next) => {
+  try {
+    const { name } = req.body
+    const team = await Team.create({ name })
+
+    res.status(201).send(team)
+  } catch (error) {
+    if (error instanceof ValidationError) error.status = 422
+    console.error(error)
+    next(error)
+  }
+}
+
+const modifyTeam = async (req, res, next) => {
+  const { name } = req.body
+
+  try {
+    const team = await Team.update(
+      { name },
+      { where: { id: req.params.id }, returning: true }
+    )
+    res.send(team)
+  } catch (error) {
+    return res.send(console.error(error)).status(400)
+  }
+}
+
+const deleteTeam = async (req, res, next) => {
+  try {
+    const team = await Team.destroy({ where: { id: req.params.id } })
+    res.sendStatus(204)
+  } catch (error) {
+    return res.send(console.error(error)).status(400)
+  }
+}
+
+module.exports = {
+  createTeam,
+  modifyTeam,
+  deleteTeam,
+  allTeam,
+  oneTeam,
+}
