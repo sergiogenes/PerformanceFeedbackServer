333a093841c3b0e49de9464f7e6e751501421cc3
Ezequiel Birman <ebirman77@gmail.com>
2023-04-02 21:22:47 -0300

    Fix foreign key name
    The foreign key was misnamed

--- models/team.js
+++ models/team.js
@@ -3,7 +3,7 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Team extends Model {
     static associate(models) {
-      Team.hasMany(models.User, { foreignKey: 'userId' })
+      Team.hasMany(models.User, { foreignKey: 'teamId' })
     }
   }
   Team.init(
