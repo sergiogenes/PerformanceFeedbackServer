de22ec2686d6da37b4f25233b285829609f38a0d
Ezequiel Birman <ebirman77@gmail.com>
2023-04-04 11:22:46 -0300

    bugfix: call correct method name `verifyToken`

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-const { validateToken } = require('../utils/token')
+const { verifyToken } = require('../utils/token')
 
 const validateAuth = (req, res, next) => {
   const token = req.cookies.token
   if (!token) return res.sendStatus(401)
 
-  const { user } = validateToken(token)
+  const user = verifyToken(token)
   if (!user) return res.sendStatus(401)
 
   req.user = user
