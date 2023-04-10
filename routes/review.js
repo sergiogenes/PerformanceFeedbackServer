8a0b5f222df00ee050817e3124b554dab11d433f
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-10 17:51:48 -0300

    fix validaciones review

--- routes/review.js
+++ routes/review.js
@@ -4,12 +4,12 @@ const {
   getReviewEvaluator,
   deleteReview,
 } = require('../controllers/review')
-
 const router = require('express').Router()
+const { validateAuth } = require('../middleware/auth')
 
-router.get('/:id', getReviewEvaluator)
-router.get('/', getReviews)
-router.post('/', createReview)
-router.delete('/:id', deleteReview)
+router.get('/:id', validateAuth, getReviewEvaluator)
+router.get('/', validateAuth, getReviews)
+router.post('/', validateAuth, createReview)
+router.delete('/:id', validateAuth, deleteReview)
 
 module.exports = router
