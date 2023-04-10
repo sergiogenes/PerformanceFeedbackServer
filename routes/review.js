ed2731e0d1a6acfd1dcaacedc58a7327832a12a8
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-10 18:28:46 -0300

    ruta obtener devolucion por evaluado

--- routes/review.js
+++ routes/review.js
@@ -2,12 +2,14 @@ const {
   getReviews,
   createReview,
   getReviewEvaluator,
+  getReviewEvaluated,
   deleteReview,
 } = require('../controllers/review')
 const router = require('express').Router()
 const { validateAuth } = require('../middleware/auth')
 
-router.get('/:id', validateAuth, getReviewEvaluator)
+router.get('/:id', validateAuth, getReviewEvaluated)
+router.get('/evaluator/:id', validateAuth, getReviewEvaluator)
 router.get('/', validateAuth, getReviews)
 router.post('/', validateAuth, createReview)
 router.delete('/:id', validateAuth, deleteReview)
